26c78e8ac7ea2de2379e5a5db03dcbcba3ce460f
MiguelMarcal <75666756+MiguelMarcal@users.noreply.github.com>
2023-07-02 03:50:19 +0100

    UI and logic changes
    Cleared some code, also changed the color of the main timer, as well as the page name

--- main.js
+++ main.js
@@ -5,67 +5,70 @@
 // The main timer has "timer" id
 
 let timerInterval; // Variable to store the interval ID
-let stopTimer = false
+let stopTimer = false;
 
 function bGMode() {
-    bgColor = document.getElementById("ld_btn")
-    if(bgColor.innerHTML === "Light Mode"){
-        document.body.style.backgroundColor = "white"
-        document.body.style.color = "black"
-        bgColor.innerHTML = "Dark Mode"
-    }else{
-        document.body.style.backgroundColor = "#545454"
-        document.body.style.color = "white"
-        bgColor.innerHTML = "Light Mode"        
-    }
+  bgColor = document.getElementById("ld_btn");
+  if (bgColor.innerHTML === "Light Mode") {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    bgColor.innerHTML = "Dark Mode";
+  } else {
+    document.body.style.backgroundColor = "#545454";
+    document.body.style.color = "white";
+    bgColor.innerHTML = "Light Mode";
+  }
 }
 
 function workChange(value) {
-    if (value.length > 1) {
-        document.getElementById("work_display").innerHTML = value  
-        document.getElementById("work_display").dataset.value = value  
-        document.getElementById("timer").innerHTML = value + ":" + "00";
-      } else if(value.length == 1){
-        document.getElementById("work_display").innerHTML = value  
-        document.getElementById("work_display").dataset.value = value  
-        document.getElementById("timer").innerHTML = "0" +  value + ":" + "00";
-      }
-      else {
-        document.getElementById("work_display").innerHTML = "00" 
-        document.getElementById("work_display").dataset.value = "00"
-        document.getElementById("timer").innerHTML = "00" + ":" + "00";
-      } 
+  work_display = document.getElementById("work_display");
+  timer = document.getElementById("timer");
+  if (value.length > 1) {
+    work_display.innerHTML = value;
+    work_display.dataset.value = value;
+    timer.innerHTML = value + ":" + "00";
+  } else if (value.length == 1) {
+    work_display.innerHTML = value;
+    work_display.dataset.value = value;
+    timer.innerHTML = "0" + value + ":" + "00";
+  } else {
+    work_display.innerHTML = "00";
+    work_display.dataset.value = "00";
+    timer.innerHTML = "00" + ":" + "00";
+  }
 }
 
 function pauseChange(value) {
-    if (value.length > 1) {
-        document.getElementById("pause_display").innerHTML = value  
-        document.getElementById("pause_display").dataset.value = value  
-      } 
-      else if(value.length == 1){
-        document.getElementById("pause_display").innerHTML = value  
-        document.getElementById("pause_display").dataset.value = value  
-        document.getElementById("timer").innerHTML = "0" +  value + ":" + "00";
-      }else {
-        document.getElementById("pause_display").innerHTML = "00" 
-        document.getElementById("pause_display").dataset.value = "00"
-      }    
-
+  pause_display = document.getElementById("pause_display");
+  if (value.length > 1) {
+    pause_display.innerHTML = value;
+    pause_display.dataset.value = value;
+  } else if (value.length == 1) {
+    pause_display.innerHTML = value;
+    pause_display.dataset.value = value;
+  } else {
+    pause_display.innerHTML = "00";
+    pause_display.dataset.value = "00";
+  }
 }
 
-function timer(time) {
-  let remainingTime = time*60;
+function startTimer(time) {
+  let remainingTime = time * 60;
   return new Promise((resolve) => {
     interval = setInterval(() => {
-      if (stopTimer) { // Check if stopTimer is true before updating the timer display
+      if (stopTimer) {
+        // Check if stopTimer is true before updating the timer display
         clearInterval(interval);
         console.log("Timer stopped");
         resolve();
       } else {
-        let minutes = (Math.floor(remainingTime / 60)).toString().padStart(2, '0'); // Get the whole minutes
-        let remainingSeconds = (remainingTime % 60).toString().padStart(2, '0'); // Get the remaining seconds
-        document.getElementById("timer").innerHTML = minutes + ":" + remainingSeconds;
-
+        let minutes = Math.floor(remainingTime / 60)
+          .toString()
+          .padStart(2, "0"); // Get the whole minutes
+        let remainingSeconds = (remainingTime % 60).toString().padStart(2, "0"); // Get the remaining seconds
+        document.getElementById("timer").innerHTML =
+          minutes + ":" + remainingSeconds;
+        document.title = minutes + ":" + remainingSeconds;
         remainingTime -= 1;
         if (remainingTime < 0) {
           clearInterval(interval);
@@ -73,49 +76,56 @@ function timer(time) {
           resolve();
         }
       }
-    }, 1000);
+    }, 100);
   });
-  }
-  
-
+}
 
 async function start() {
-  playSound("assets/tap.mp3")
+  playSound("assets/tap.mp3");
   stopTimer = false;
   workTime = document.getElementById("work_display").dataset.value;
   pauseTime = document.getElementById("pause_display").dataset.value;
   document.getElementById("start_btn").style.display = "none";
   document.getElementById("stop_btn").style.display = "block";
-
+  timer = document.getElementById("timer");
   count = 1;
-
+  timer.classList.add("border-success");
   while (!stopTimer) {
     if (count % 2 == 0) {
-      await timer(parseInt(pauseTime));
-      await playSound("assets/fart.mp3");
+      await startTimer(parseInt(pauseTime));
+      await playSound("assets/work.mp3");
       popup("Timer to work Bitch");
+      timer.classList.toggle("border-warning");
+      timer.classList.toggle("border-success");
     } else {
-      await timer(parseInt(workTime));
-      await playSound("assets/fart.mp3");
+      await startTimer(parseInt(workTime));
+      await playSound("assets/pause.mp3");
       popup("You can chill now");
+      timer.classList.toggle("border-warning");
+      timer.classList.toggle("border-success");
     }
     count += 1;
     console.log(count);
   }
+  timer.classList.remove(
+    timer.classList.contains("border-warning")
+      ? "border-warning"
+      : "border-success"
+  );
 }
 
 function stop() {
-  playSound("assets/tap.mp3")
+  playSound("assets/tap.mp3");
   document.getElementById("start_btn").style.display = "block";
   document.getElementById("stop_btn").style.display = "none";
   stopTimer = true;
   clearInterval(interval); // Clears the interval, stopping the timer
-  document.getElementById("timer").innerHTML = document.getElementById("work_display").dataset.value;
+  document.title = "Pomodoro Work";
 }
 
-function popup(text){
-  if(!confirm(text)){
-    stop()
+function popup(text) {
+  if (!confirm(text)) {
+    stop();
   }
 }
 
